620e5d44ee25a26580ad7f3f4c2c99a732941d66
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-20 13:53:35 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** 
+ * Main -entry point
+ * Description: print using printf
+ * Return: 0 
+*/
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
