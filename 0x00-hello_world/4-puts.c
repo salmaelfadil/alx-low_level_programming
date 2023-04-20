260eae313b97f105e4a1bb6bb03bfa62b42cf5a8
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-20 13:48:47 -0700

    ff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -6,8 +6,8 @@
  * Return: 0
  */
 
-int main(void) 
-{ 
-	puts("\"Programming is like building a multilingual puzzle"); 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
