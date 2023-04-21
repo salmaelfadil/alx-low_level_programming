fecd2279eb0d8a70f158e191856ee0f4d09f457b
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 16:27:36 -0700

    yy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stido.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * description: print alphabet
+ * return:0
+ */
+int main(void)
+{
+	char a='a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
