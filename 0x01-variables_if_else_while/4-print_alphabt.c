07628e2a79408527b0978edf903733baaa3cfe1c
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 16:49:12 -0700

    zz

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: print alphabet except q e
+ * return: 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
