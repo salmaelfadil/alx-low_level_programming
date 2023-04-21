3fbe183941d442f966a53e91a5c6cf35f3607ed2
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 15:39:20 -0700

    style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/** 
- * main - Entry poing
+/**
+ * main - Entry point
  * Description: positive/negative/zero
  * Return: 0
  */
@@ -16,11 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n == 0)
 		printf ("%i is zero\n", n);
-	else 
+	else
 		printf("%i is negative\n", n);
-	return (0);
+	return(0);
 }
