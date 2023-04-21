63ad000dce3c413f92be056890f03b12900ccf3d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 16:54:44 -0700

    ss

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i",i);
+	printf("\n");
+	return (0);
+}
