209ea6cf0722dd8e8d241c3c3a3ccca778fe945d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 17:08:19 -0700

    hh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char z = 'z';
+
+	while (z => 'a')
+	{
+		putchar(z);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
