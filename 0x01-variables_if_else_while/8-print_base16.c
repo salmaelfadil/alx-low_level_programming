302f265cc652a0bec264504eb5505909b38e163a
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 17:14:25 -0700

    dd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	char a;
+
+	for(i = 48; i < 58; i++)
+		putchar(i);
+	for(a = 'a'; a < 'g'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
