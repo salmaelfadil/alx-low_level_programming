8abaa39d7995e31718d542bbae52ad1be2c8802f
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-21 17:28:50 -0700

    ee

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stido.h>
+/**
+ * main - entry point
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
