e9aa2ea89b1d5d8722a85e9f5c259027793ab675
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 14:54:59 -0700

    r

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,12 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n < 98; n++)
+		for (; n < 99; n++)
 			printf("%d, ", n);
 	}
 	else if (n > 98)
 	{
-		for (; n > 98; n++)
+		for (; n > 99; n++)
 			printf("%d, ", n);
 	}
 	else if (n == 98)
