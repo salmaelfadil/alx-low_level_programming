06505c3468fe1c2d287483b4545e5378a938f531
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 13:02:02 -0700

    rr

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_sign - checks the sign of a number
+  * @n: number to check
+  *
+  * Return: 1 for positive num, -1 for negative num or zero for 0
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('1';
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar('1');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
