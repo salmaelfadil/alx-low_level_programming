c3eb84de93edc2674f8624adb9a8f2a27fa00e9f
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 11:49:30 -0700

    r

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
-/** main - entry point
- * 
- * Return: 0 
+/**
+ * main - entry point
+ * print string
+ * Return: 0
  */
-
 int main(void)
 {
-	char word[]="_putchar";
+	char word[] = "_putchar";
 	int i;
 
-	for (i=0; i <8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		putchar(word[i]);
 	}
