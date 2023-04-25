63bee05ee0fb35d9ba79bdd4f2ebc7d71d4f143d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 15:12:30 -0700

    rr

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_times_table - print times table from 0
+ * @n: the number up untill times table will be printed
+ *
+ * return: voidl
+ */
+void print_times_table(int n)
+{
+	int i, j, m;
+
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j < n; j++)
+                {
+                        m = i * j;
+                        if (m < 10)
+                        {
+                                if (j != 0)
+                                {
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                }
+                                _putchar(m + '0');
+                        }
+                        else
+                        {
+                                _putchar(' ');
+                                _putchar((m / 10) + '0');
+                                _putchar((m % 10) + '0');
+                        }
+                        if (j != 9)
+                                _putchar(',');
+                }
+                _putchar('\n');
+        }
+}
+
+~
