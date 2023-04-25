243c4aae75dd4ca1c29079aabe804dc4308f4d71
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 14:21:14 -0700

    p

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,19 @@
  */
 void times_table(void)
 {
-	int i,j,n,m;
+	int i, j, n, m;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			for(n = 0; n < 10; n++)
+			for (n = 0; n < 10; n++)
 			{
 				m = j * n;
-				_putchar(m);
+				_putchar('m');
 				_putchar(',');
 			}
 		}
 	_putchar('\n');
 	}
-	
 }
