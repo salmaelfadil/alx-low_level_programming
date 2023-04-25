fe44d78e2dec26beaaf5395af96573c2450df9e6
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 12:13:09 -0700

    r

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - main entry point
+ * checks if character is lowercase
+ * Return : 0 or 1 
+ */
+
+int _islower(int c)
+{
+	char c;
+	if c < 97
+		return 0;
+	else 
+		return 1; 
+}
