982aece43b19e2ee74aa5db8fe0591c2a5f876d1
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 15:18:25 -0700

    rr

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,30 +8,30 @@
 void print_times_table(int n)
 {
 	int i, j, m;
-
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 0; j < n; j++)
-                {
-                        m = i * j;
-                        if (m < 10)
-                        {
-                                if (j != 0)
-                                {
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                }
-                                _putchar(m + '0');
-                        }
-                        else
-                        {
-                                _putchar(' ');
+	
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			m = i * j;
+			if (m < 10)
+			{
+				if (j != 0)
+				{
+				       	_putchar(' ');
+				       	_putchar(' ');
+				}
+				_putchar(m + '0);
+			}
+			else
+			{
+			       	_putchar(' ');
                                 _putchar((m / 10) + '0');
                                 _putchar((m % 10) + '0');
                         }
-                        if (j != 9)
-                                _putchar(',');
-                }
-                _putchar('\n');
-        }
+			if (j != 9)
+			       	_putchar(',');
+		}
+		_putchar('\n');
+	}
 }
