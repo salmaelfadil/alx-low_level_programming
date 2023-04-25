423e03d411d1bad91267317763885268134e94f6
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 14:13:23 -0700

    rr

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - print time table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i,j,n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			for(n = 0; n < 10; n++)
+			{
+				_putchar('j * n');
+				_putchar(',');
+			}
+		}
+	_putchar('\n');
+	}
+	
+}
