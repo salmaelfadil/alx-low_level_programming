30e412d5134d86de6574a0830da2f65dc18e0bb7
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 14:01:45 -0700

    rr

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,14 +14,14 @@ void jack_bauer(void)
 		min = 0;
 		while (min < 60)
 		{
-			_putchar((hour / 19) + '0');
+			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');
-			min += 1;
+			min++;
 		}
-		hour += 1;
+		hour++;
 	}
 }
