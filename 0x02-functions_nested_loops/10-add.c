17325ba48ccfb89a8ce2d5b115f72e7d1ef93551
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 14:36:26 -0700

    rr

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function adds to ints
+ * @i1: int to be added
+ * @i2: int to be added
+ *
+ * Return: added number
+ */
+
+int add(int i1, int i2)
+{
+	numb = i1 + i2;
+
+	_putchar(numb);
+}
