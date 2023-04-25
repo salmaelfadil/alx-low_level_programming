5c02aee39a73dde106627d1fbfcb592f84d5b347
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 13:10:52 -0700

    rr

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of an integer
+ * @c: int whose absolute value will be taken
+ *
+ * Return: abs value
+ */
+int _abs(int c)
+{
+	return (c *-1);
+}
