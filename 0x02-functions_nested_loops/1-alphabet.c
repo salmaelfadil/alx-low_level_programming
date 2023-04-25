23872b640931f9fe3afb30995729e7776a4b57cf
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 11:58:10 -0700

    r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print alphabet
+ * Return: void
+ */
+void  main(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
