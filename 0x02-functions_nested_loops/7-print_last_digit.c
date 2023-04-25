116baa50560c9ca85b18ca30771768be6ee37db7
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 13:15:13 -0700

    r

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of num
+ * @i: num whose last digit will be printed
+ *
+ * Return: last digit of num
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
