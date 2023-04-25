2216b9dedd51bfe342b6ad58cb8ec0b0e43af165
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-25 12:05:18 -0700

    r

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ * print alphabet 10 times
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int l;
+	int i;
+
+	for (l = 0; l < 11; l ++)
+	{
+		for (i = '97'; i < 123; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}
