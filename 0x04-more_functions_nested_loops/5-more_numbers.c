aa019da93d56716a5cb5188aff16dfc51eeb42da
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 09:38:03 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, x;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (x = 0; x <= 15; x++)
+		{
+			if (x > 9)
+			{
+				_putchar((x / 10) + '0');
+			}
+		}
+		_putchar((x % 10) + '0');
+	}
+
+	_putchar('\n');
+}
+
