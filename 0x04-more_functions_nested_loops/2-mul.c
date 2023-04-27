4a6dbc3d64c9456549e9fd883d5d7e2559ec4c31
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 09:00:35 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: multiplied num
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+
+	return (m);
+}
