34b6b1b455740e51b2b7ae6af81b55b15ea63386
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 10:01:46 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal line
+ * @n: number of times \ would be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i,j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			-putchar('\n');
+		}
+	}
+}
