c60b2426a1dbcb2eb661e4387469642f7df3a75a
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 09:54:20 -0700

    ..

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,18 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('-');
+		_putchar('\n');
 	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 
 	_putchar('\n');
+	}
 }
 
