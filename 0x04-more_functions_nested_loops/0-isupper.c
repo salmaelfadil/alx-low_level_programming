244d300b0fe349ca2d4421e92b1a75a70256c75c
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 08:31:16 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - checks if character is uppercase
+ * @c: character to be checked
+ * Return:1 if uppercase and 0 if lowecase
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) || (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
