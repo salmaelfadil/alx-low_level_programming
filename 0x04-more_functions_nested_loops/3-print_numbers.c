129e757ff8351b171e0252f2f92eddddb82d4262
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 09:05:54 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar("%i", i);
+	_putchar("\n");
+	return (0);
+}
