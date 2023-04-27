fcb3039f2c769c0c64cfdd1e1b9ce05b18b9ce51
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 09:32:19 -0700

    ..

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_most_numbers - print numbers from 0 to 9
  *
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
