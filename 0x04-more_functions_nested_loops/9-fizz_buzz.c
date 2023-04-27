5a876c6b9118565562e195a18dfedbc06901fcfe
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-04-27 16:25:12 -0700

    ..

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ void main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0) && (i % 5 == 0)
 		{
@@ -30,6 +30,10 @@ void main(void)
 		else
 		{
 			printf("%d", i)
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
