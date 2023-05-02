bd9572aa5b0e4baf2ea65c204be005957a82e306
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-02 12:24:38 -0700

    ..

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two ints
+ * @a: int 1 
+ * @b: int 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *c = *a;
+	int *a = *b;
+	int *b = *c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+#endif
