2fbb5d65f99fe07d16c53128d84df15fd6fbd655
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-02 16:01:27 -0700

    .

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,14 @@
 char *_strcpy(char *dest, char *src)
 {
 
-	int i = 0, j = 0;
+	int i = 0;
 
 	while (src[i] != '\0')
 	{
-		dest[j] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	dest[j++] = '\0';
+	dest[i++] = '\0';
 
 	return (dest);
 }
