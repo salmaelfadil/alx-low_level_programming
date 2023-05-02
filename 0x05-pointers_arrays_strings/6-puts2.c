139f74056347d2ec989778f769e257197c04dbe7
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-02 14:46:28 -0700

    .

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: string to be printed
+ *
+ * Return: void;
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] = '\n'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
