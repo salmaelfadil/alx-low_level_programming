cc7834f812af127dbf15c87d6e4782c88e7d9e78
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-02 12:38:18 -0700

    ..

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- * _strlen: returns length of string
+ * _strlen - returns length of string
  * @s: string whose length will be returned
  *
+ * Return: String length
  */
 int _strlen(char *s)
 {
 	int count = 0;
-	for(int i = 0; *s != '\0'; i++)
+	int i = 0;
+
+	for (; *s != '\0'; i++)
 	{
 		count++;
 	}
-	return count; 
+	return (count);
 }
