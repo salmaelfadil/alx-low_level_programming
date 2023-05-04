b705b58958a0bc5a48d10c6a6ad66ee07cbc835e
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-04 12:18:08 -0700

    .

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - makes all lowercase letters uppercase
+ * @s: pointer to string
+ *
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		else
+			continue
+	}
+	return (s);
+}
