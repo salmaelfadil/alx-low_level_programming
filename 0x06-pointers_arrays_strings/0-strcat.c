aa9ac8df71c0e6fdc5bc1face2c53ae54a1d8b5d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-04 09:40:04 -0700

    .

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ *
+ * Return: pointer to string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[j])
+	{
+		j++;
+	}
+	for (; src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
