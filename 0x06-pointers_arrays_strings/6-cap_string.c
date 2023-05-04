f4ae0655c4bb7c07314afe7f5460e2a57ad9cb41
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-04 12:41:55 -0700

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,25 @@
 #include "main.h"
-/** 
+/**
  * cap_string - capitalizes all words of a string
  * @s: string to be capitalized
  *
  * Return: capitalized string
  */
-
 char *cap_string(char *s)
 {
 	int i = 0;
-	
+
 	for (; s[i] != '\0'; i++)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			s[0] = s[0] - 32;
+			s[i] = s[i] - 32;
 		}
-		else if (s[i] == ' ' || s[i] = '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
+				|| s[i] == ',' || s[i] == ';' || s[i] == '.'
+				|| s[i] == '!' || s[i] == '?' || s[i] == '"'
+				|| s[i] == '(' || s[i] == ')' || s[i] == '{'
+				|| s[i] == '}')
 		{
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 			{
