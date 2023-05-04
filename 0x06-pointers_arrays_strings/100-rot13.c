89a808632bd1c025e61161fcdca21eb4cb039232
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-04 16:15:51 -0700

    .

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rot13 - replace the letter by the 13th letter after it
+ * @s: string which will be ciphered
+ *
+ * Return: ciphered string
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 65 && s[i] <= 90 || s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] + 13;
+		}
+	}
+	return (s);
+}
