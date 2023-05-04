d51aa41d52e005e514b5d76bcd0ec32212c40dab
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-04 16:38:09 -0700

    .

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,13 +9,12 @@ char *rot13(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	for (; s[i] !='\0'; i++)
 	{
 		if (s[i] >= 65 && s[i] < 91 || s[i] >= 97 && s[i] < 123)
 		{
 			s[i] = s[i] + 13;
 		}
-		i++;
 	}
 	return (s);
 }
