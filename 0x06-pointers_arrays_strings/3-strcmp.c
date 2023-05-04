575139664e1ead258e262adfa5699610b71506f9
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-04 15:12:16 -0700

    .

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j;
 
-	while (s1[i])
+	while (s1[i] != '\0')
 	{
 		c1++;
 		i++;
@@ -19,10 +19,10 @@ int _strcmp(char *s1, char *s2)
 
 	j = 0;
 
-	while (s2[j])
+	while (s2[j] != '\0')
 	{
 		j++;
 	}
 
-	return (i - j);
+	return (j - i);
 }
