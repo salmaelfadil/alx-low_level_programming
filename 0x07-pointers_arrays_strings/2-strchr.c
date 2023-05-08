9cd03c270a604586d1017e9236152c4f756a5034
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-08 12:56:51 -0700

    .

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (; s[i] != '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-		else
-			return (NULL);
 	}
 	return (NULL);
 }
