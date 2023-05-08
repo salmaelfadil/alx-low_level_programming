100ac73c0fd501fba64366c4da8ba316c5a91972
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-08 12:23:48 -0700

    .

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to be searched for
+ * Return: pointer to character
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	int *ptr = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			*ptr = s[i];
+			return (ptr);
+		}
+
+		else
+			return (NULL);
+	}
+}
