3fd7d1054feeec161f4052c0f2124118852fef15
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-08 13:34:47 -0700

    .

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,9 +11,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j = 0;
 
-	for (; s[i] >= '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
-		for (; accept[j] >= '\0'; j++)
+		for (; accept[j] != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
