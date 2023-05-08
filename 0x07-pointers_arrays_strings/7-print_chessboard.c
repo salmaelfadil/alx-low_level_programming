54f6c7577ad4ae87795184e8810b0df83e0abf9b
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-08 14:16:30 -0700

    .

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i = 0, j = 0;
 
-	for (; i <= 8; i++)
+	for (; i < 8; i++)
 	{
-		for (; j <= 8; j++)
+		for (; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
