8f1cd3cac3b7f6054abe29a3258011845cec865d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-12 09:42:49 -0700

    .

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,9 +4,11 @@
  * @argc: argument count
  * @argv: argument value
  *
- * Return: void
+ * Return: 0 success
  */
-void main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
+
+	return (0);
 }
