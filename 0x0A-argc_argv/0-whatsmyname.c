23a01dab9b2940fbbd6e40dbd4d8c6a909382726
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-12 09:19:35 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the program's name followed by new line
+ * @argc: argument vount
+ * @argv: argument value
+ *
+ * Return: void
+ */
+void main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}	
