296f2278cbc09bb40bcaf8e94b804028a193cde8
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-12 10:07:13 -0700

    .

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - adds two positive numbers
  * @argc: argument count
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
 
 	for (; i < argc; i++)
 	{
-		if (argv[i] > '9' || argv[i] < '0')
+		if (!isdigit(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
