51991c4059cf0baa81e30838b4c1299a858425b1
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-15 07:33:58 -0700

    .

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,33 +11,30 @@ char *argstostr(int ac, char **av)
 {
 	int i = 0, j = 0, len = 0;
 	char *a;
-	
+
 	if (ac == 0 || av == 0)
 		return (NULL);
-	
+
 	for (; i < ac; i++)
 	{
-		while (av[i][j])
-		{
+		for (; av[i][j]; j++)
 			len++;
-			j++;
-		}
 	}
-	
+
 	a = malloc(sizeof(char) * (len + 1));
 
 	if (a == NULL)
 		return (NULL);
 
-	for(i = 0; i <= ac; i++)
-	{	
+	for (i = 0; i <= ac; i++)
+	{
 		for (j = 0; av[i][j]; j++)
 			a[i] = av[i][j];
-		
+
 		a[i + 1] = '\n';
 	}
-	
+
 	a[i] = '\0';
-	
+
 	return (a);
 }
