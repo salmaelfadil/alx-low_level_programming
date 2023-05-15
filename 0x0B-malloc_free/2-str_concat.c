3c811ca8cb716cd4c8eec3d3b2883abe1e8ae066
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-15 02:07:42 -0700

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,12 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int k, len = 0, i = 0, j = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 ="";
+
 	while (s1[i] || s2[i])
 	{
 		len++;
