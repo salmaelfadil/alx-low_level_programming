acd584844569ee0462cdc037623f7bb9284de4ce
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-18 15:18:03 -0700

    .

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (; i < (nmemb * size); i++)
+	for (; nmemb[i]; i++)
 		ptr[i] = 0;
 	
 	return (ptr);
