41ae929b952a52ca129e2177081cd1e5aa7be80a
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-22 01:59:53 -0700

    .

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -- function that prints the name of the file it was complied from
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
