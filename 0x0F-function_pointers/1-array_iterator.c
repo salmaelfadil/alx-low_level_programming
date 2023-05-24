f1b90b19890df9067c8c04744094524c9146432e
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-24 03:11:30 -0700

    .

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t int i = 0;
+	unsigned int i = 0;
 
 	if (array != NULL & action != NULL)
-		for(; i <= size; i++)
+		for(; i < size; i++)
 			action(array[i]);
 }
