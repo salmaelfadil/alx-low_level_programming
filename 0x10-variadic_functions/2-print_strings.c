ec107a2570e4d295d4fa7356901ce917e15e44b9
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-25 15:16:15 -0700

    .

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nil");
 		else
 			printf("%s", s);
-		if (flag == true && i < n - 1)
+		if (flag == true && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
