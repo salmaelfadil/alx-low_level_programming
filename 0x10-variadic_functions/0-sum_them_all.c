ca4a612f31603b11ee3114c5d3cbd4b11357a27a
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-25 05:11:18 -0700

    .

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all -- function that returns the sum of all its parameters
+ * @n: number of parameters used
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int result;
+	unsigned int i = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_list np;
+
+	va_start(np, n);
+
+	for (; i < n; i++)
+		result += va_arg(np, int);
+
+	va_end(np);
+
+	return (result);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdarg.h> 
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
