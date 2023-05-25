fb06a15dd587af2ea730fec9b97a44972c328afc
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-25 06:14:47 -0700

    .

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+/**
+ * print_all -- function that prints anything
+ * @format: list of all types of arguments
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *s;
+	va_list list;
+
+	va_start(list, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(list, int));
+			case 's':
+				s = va_arg(list, char*);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+			default:
+				if (format[i + 1] != '\0')
+					printf(", ");
+				i++;
+				continue;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
