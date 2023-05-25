f378c047f8cdf1c4580ac267ffd4499a1d4be2cf
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-25 05:25:34 -0700

    .

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers -- prints numbers 
+ * @separator: string to be printed between numbers
+ * @n: number of ints
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	bool flag = true;
+	va_list np;
+
+	va_start(np, n);
+
+	if (separator == NULL)
+		flag = false;
+
+	for (; i < n; i++)
+	{
+		printf("%d", va_arg(np, int));
+		if (flag == true)
+			printf("%s", separator);
+
+	}
+
+	printf("\n");
+
+	va_end(np);
+}
