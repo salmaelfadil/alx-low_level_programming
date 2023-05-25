517496b1d1813665007664d937d42152966d4c9b
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-05-25 15:46:35 -0700

    .

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,33 +10,37 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *s;
 	va_list list;
+	bool flag;
 
 	va_start(list, format);
 
-	if (format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
-		while (format[i] != '\0')
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(list, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(list, double));
-					break;
-				case 's':
-					s = va_arg(list, char *);
-					if (s == NULL)
-						s = "(nil)";
-					printf("%s", s);
-					break;
-			}
-			printf(", ");
-			i++;
+			case 'c':
+				printf("%c", va_arg(list, int));
+				flag = true;
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				flag = true;
+				break;
+			case 'i':
+				printf("%i", va_arg(list, int));
+				flag = true;
+				break;
+			case 's':
+				s = va_arg(list, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
 		}
-		printf("\n");
-		va_end(list);
+		if (format[i + 1] != '\0' && flag = true)
+			printf(", ");
+		i++;
 	}
-
+	printf("\n");
+	va_end(list);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
