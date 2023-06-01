8ade057b998adde31dc6d8140fcf737e9dccb178
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-01 10:26:53 -0700

    .

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_node - function that add a new node at the beginning list
+ * @head: dereferenced pointer to head
+ * @str: string to be duplicated
+ *
+ * Return: address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	int i = 0;
+	struct list_t *head = NULL;
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->str = strdup(str);
+
+	for (; str[i] != NULL; i++)
+		;
+
+	node->len = i;
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
