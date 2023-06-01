9ca7bb90f5cffffd3aa242f2187f68748cf695cf
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-01 10:59:34 -0700

    .

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list
+ * @head: pointer to head of list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}
