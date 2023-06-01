84c9a2c7461e61a09981cfc50af59963e18c2d88
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-01 10:43:58 -0700

    .

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_node_end -- function that adds a new node at the end of a list
+ * @head: head of the list
+ * @str: string to be duplicated
+ *
+ * Rereturn: address of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *node;
+
+	if (str == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->str = strdup(str);
+
+	for (; str[i]; i++)
+		;
+
+	node->len = i;
+	node->next = NULL;
+	*tail = node;
+
+	return (node);
+}
