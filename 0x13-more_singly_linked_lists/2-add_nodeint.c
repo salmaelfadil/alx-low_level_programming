1250773764df96483a2b08fa33f950d456d8fa20
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-05 03:38:17 -0700

    .

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/*
+ * *add_nodeint -- adds a node to the beggining of a list
+ * @head: head of the list
+ * @n: number to be added
+ *
+ * Return: list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *node;
+
+	node = malloc(sizeof(listint_t));
+	
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_nodeint_end -- function that adds node at the end of the list
+ * @head: head of the list
+ * @n: int to be added
+ *
+ * Return: address of new element
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *node;
+	listint_t *tmp;
+
+	node = malloc(sizeof(listint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	tmp = *head;
+	
+	while (tmp->next)
+	{
+		tmp = tmp->enxt;
+	}
+	
+	tmp->next = node;
+	
+	return (node);
+}
