1064cd6049c8939d2906a304d1ddbf1ad9dad528
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-06 01:09:18 -0700

    .

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,10 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
-		*head = next;
+		*head = tmp;
 	}
-
-	*head = NULL;
 }
