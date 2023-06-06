79b1edca0b2909b8249e0925ad9acb8332690d43
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-06 01:07:52 -0700

    .

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint2 -- frees a listint_t list
- * @head
+ * @head: pointer to the head of the list
  *
  * Return: void
  */
@@ -18,4 +18,6 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = next;
 	}
+
+	*head = NULL;
 }
