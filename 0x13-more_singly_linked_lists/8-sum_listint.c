f5851d6dd10bda25f05512a0ff45570a014aef47
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-06 01:15:46 -0700

    .

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,14 +3,11 @@
  * sum_listint -- returns sum of all data in list
  * @head: head of list
  *
- * Return: sum
+ * Returin: sum
  */
 int sum_listint(listint_t *head)
 {
-	int sum;
-
-	if (head == NULL)
-		return (0);
+	int sum = 0;
 
 	while (head)
 	{
