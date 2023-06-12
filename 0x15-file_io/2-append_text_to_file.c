217a1eb4d015ff2944e7234b8df8cfd3574f6df1
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-12 06:53:59 -0700

    .

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,10 +22,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[i])
 			i++;
-	}
 
-	if (i > 0)
 		w = write(fd, text_content, i);
+	}
 
 	if (w == -1)
 		return (-1);
