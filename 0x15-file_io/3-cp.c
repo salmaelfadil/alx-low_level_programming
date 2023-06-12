229658ec62bf9f9d9fb559d5b459b1255861637c
salmaelfadil <salmaelfasdil@aucegypt.edu>
2023-06-12 13:37:43 -0700

    .

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,15 +13,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 	f1 = open(argv[1], O_RDONLY);
+
 	if (f1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
 	if (f2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+
 	while ((r = read(f1, buffer, 1024)) > 0)
 		if (write(f2, buffer, r) != r)
 		{
