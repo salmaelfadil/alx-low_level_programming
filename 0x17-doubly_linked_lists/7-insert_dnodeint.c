7e1eab69c0a2ce10e58bc2a5a019906a347bc22c
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-20 14:05:36 -0700

    .

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,45 +2,36 @@
 /**
  * insert_dnodeint_at_index -- inserts node at index
  * @h: double pointer to head of list
+ * @idx: index of node
  * @n: number to be added in the list
  * Return: address of the new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
+	dlistint_t *new = NULL;
 	dlistint_t *head = *h;
-	unsigned int size = 0;
-	unsigned int i = 0;
-
-	new = NULL;
+	unsigned int size, i = 0;
 
 	if (!h)
 		return (NULL);
-
 	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
 	}
-	for (; head != NULL; size++)
+	for (size = 0; head != NULL; size++)
 		head = head->next;
-
 	if (idx > size)
 		return (NULL);
-
 	head = *h;
-
 	if (idx == size)
 	{
 		new = add_dnodeint_end(h, n);
 		return (new);
 	}
-
 	new = malloc(sizeof(dlistint_t));
-
 	if (!new)
 		return (NULL);
-
 	for (; i < idx - 1; i++)
 	{
 		if (head ==  NULL)
@@ -48,16 +39,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			free(new);
 			return (NULL);
 		}
-		head= head->next;
+		head = head->next;
 	}
-
 	new->n = n;
 	new->next = head->next;
 	new->prev = head;
-
 	if (head->next)
 		head->next->prev = new;
 	head->next = new;
-
 	return (new);
 }
