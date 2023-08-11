42eedf10a523f62c2d2d784e1faf07aca0deb9b6
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-11 10:40:49 -0700

    .

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ *
+ * Return: pointer to new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int i = 0;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+
+	if (!hash_table->array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+		hash_table->array[i] = NULL;
+
+	return (hash_table);
+
+}
