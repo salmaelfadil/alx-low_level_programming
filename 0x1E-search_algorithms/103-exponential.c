c36f1d21191c54753ee095d8c04b6309e4d6efd6
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-15 12:55:06 -0800

    .

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -37,3 +37,32 @@ int exponential_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, size - 1);
 	return (binary_search_recursive(array, i / 2, min(i, size - 1), value));
 }
+/**
+ * binary_search_recursive -- recusion
+ * @low: start int
+ * @high: last int
+ * @array: pointer to the first element of the array
+ * @value: value to search for
+ * Return: -1 if faliure, else return index
+*/
+int binary_search_recursive(int *array, size_t low, size_t high, int value)
+{
+	if (high >= low)
+	{
+		int mid = low + (high - low) / 2;
+		size_t i = low;
+
+		printf("Searching in array: ");
+		for (; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] > value)
+			return (binary_search_recursive(array, low, mid - 1, value));
+
+		return (binary_search_recursive(array, mid + 1, high, value));
+	}
+	return (-1);
+}
