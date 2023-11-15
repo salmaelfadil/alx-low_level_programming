0257c8c9b6ff824ae67b4613bd8135677e27418d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-15 09:54:14 -0800

    .

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,22 +9,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (!array || size == 0)
-    {
-        return (-1);
-    }
+	if (!array || size == 0)
+		return (-1);
 
-    for (; i < size; i++)
-    {
-        
-        printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 
-        if (array[i] == value)
-        {
-            return(i);
-        }
-    }
-    return (-1);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,16 +8,10 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-
-    if (!array || size == 0)
-    {
-        return (-1);
-    }
-
-    return binary_search_recursive(array, 0, size - 1, value);
- 
+	if (!array || size == 0)
+		return (-1);
+	return (binary_search_recursive(array, 0, size - 1, value));
 }
-
 /**
  * binary_search_recursive -- recusion
  * @low: start int
@@ -28,30 +22,24 @@ int binary_search(int *array, size_t size, int value)
 */
 int binary_search_recursive(int *array, size_t low, size_t high, int value)
 {
-    if (high >= low) 
-    {
-        int mid = low + (high - low) / 2;
-        
-        if (array[mid] == value)
-        {
-            return mid;
-        }
-        
-        printf("Searching in array: %d", array[low]);
-        for (size_t i = low + 1; i <= high; i++)
-        {
-            printf(", %d", array[i]);
-        }
-        printf("\n");
+	if (high >= low)
+	{
+		int mid = low + (high - low) / 2;
+		size_t i = low + 1;
 
-        if (array[mid] > value)
-        
-        {
-            return binary_search_recursive(array, low, mid - 1, value);
-        }
-        
-        return binary_search_recursive(array, mid + 1, high, value);
-        }
+		if (array[mid] == value)
+				return (mid);
 
-  return -1;
+		printf("Searching in array: %d", array[low]);
+
+		for (; i <= high; i++)
+			printf(", %d", array[i]);
+		printf("\n");
+
+		if (array[mid] > value)
+			return (binary_search_recursive(array, low, mid - 1, value));
+
+		return (binary_search_recursive(array, mid + 1, high, value));
+	}
+	return (-1);
 }
